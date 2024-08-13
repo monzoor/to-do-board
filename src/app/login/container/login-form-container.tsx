e2389f24eafd939770feab0304c62e8e2e8e3989
Monzoor Moshed Mahmud <mmtamal@gmail.com>
2024-08-13 21:09:20 +0600

    add loging form with validation

---
+++ src/app/login/container/login-form-container.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { IFormInputs } from "../types/login";
+import { loginSchema } from "../validation/login-validation";
+
+export const LoginContainer = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const onSubmit = (data: IFormInputs) => {
+    console.log(data);
+  };
+
+  return (
+    <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label
+          htmlFor="email"
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          placeholder="name@company.com"
+          {...register("email")}
+        />
+        <p className="mt-3 text-xs text-rose-400">{errors.email?.message}</p>
+      </div>
+      <div>
+        <label
+          htmlFor="password"
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="••••••••"
+          className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          {...register("password")}
+        />
+        <p className="mt-3 text-xs text-red-400">{errors.password?.message}</p>
+      </div>
+
+      <button
+        type="submit"
+        className="focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4"
+      >
+        Sign in
+      </button>
+      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        Don’t have an account yet?{" "}
+        <a
+          href="/signup"
+          className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+        >
+          Sign up
+        </a>
+      </p>
+    </form>
+  );
+};
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+import { LoginContainer } from "./container/login-form-container";
+
 export default function Login() {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
@@ -7,56 +9,7 @@ export default function Login() {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                  placeholder="name@company.com"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                  required
-                />
-              </div>
-
-              <button
-                type="submit"
-                className="focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4"
-              >
-                Sign in
-              </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
-                <a
-                  href="/signup"
-                  className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
-                >
-                  Sign up
-                </a>
-              </p>
-            </form>
+            <LoginContainer />
           </div>
         </div>
       </div>
