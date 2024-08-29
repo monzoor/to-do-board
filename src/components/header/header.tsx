1b9068c2fe5529b0f9a0b10767806fc5a6f35f21
Monzoor Moshed Mahmud <mmtamal@gmail.com>
2024-08-29 18:43:23 +0600

    add logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,7 +8,30 @@ export const Header = () => {
   return (
     <div className="relative flex h-12 w-screen items-center justify-center bg-white text-center font-bold shadow-md">
       <h1 className="absolute left-1/2 -translate-x-1/2 transform">
-        TODO BOARD
+        <svg width="220" height="48" xmlns="http://www.w3.org/2000/svg">
+          <rect
+            x="0"
+            y="4"
+            width="40"
+            height="40"
+            rx="8"
+            ry="8"
+            fill="#0079BF"
+          />
+          <rect x="10" y="14" width="8" height="20" fill="white" />
+          <rect x="24" y="14" width="8" height="14" fill="white" />
+
+          <text
+            x="52"
+            y="34"
+            fontFamily="Verdana, Geneva, sans-serif"
+            fontSize="20"
+            fontWeight="bold"
+            fill="#0079BF"
+          >
+            TODO Board
+          </text>
+        </svg>
       </h1>
       {isAuthenticated && (
         <a
