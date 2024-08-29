7e41604e0d872361bc1929a88aadc14fd6a9f6ca
Monzoor Moshed Mahmud <mmtamal@gmail.com>
2024-08-30 01:23:45 +0600

    fix style

--- src/app/components/ticket-details/ticket-details.tsx
+++ src/app/components/ticket-details/ticket-details.tsx
@@ -29,102 +29,105 @@ export const TicketDetails = ({
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col items-start bg-slate-200 p-4">
-        <div className="pb-3 font-bold">
-          Title{" "}
-          {currentDraft?.title &&
-            currentDraft.title !== ticket.title &&
-            "(draft)"}
+      <div className="h-[276px] overflow-y-auto">
+        <div className="flex flex-col items-start bg-slate-200 p-4">
+          <div className="pb-3 font-bold">
+            Title{" "}
+            {currentDraft?.title &&
+              currentDraft.title !== ticket.title &&
+              "(draft)"}
+          </div>
+          <Controller
+            name="title"
+            control={control}
+            render={({ field }) =>
+              isEditingTitle ? (
+                <input
+                  {...field}
+                  className="w-full rounded border px-2 py-1"
+                  type="text"
+                  onBlur={() => setIsEditingTitle(false)}
+                />
+              ) : (
+                <div
+                  className="w-full cursor-pointer"
+                  onClick={() => setIsEditingTitle(true)}
+                >
+                  {ticket.title}
+                </div>
+              )
+            }
+          />
         </div>
-        <Controller
-          name="title"
-          control={control}
-          render={({ field }) =>
-            isEditingTitle ? (
-              <input
-                {...field}
-                className="w-full rounded border px-2 py-1"
-                type="text"
-                onBlur={() => setIsEditingTitle(false)}
-              />
-            ) : (
-              <div
-                className="w-full cursor-pointer"
-                onClick={() => setIsEditingTitle(true)}
-              >
-                {ticket.title}
-              </div>
-            )
-          }
-        />
-      </div>
 
-      <div className="flex flex-col items-start bg-slate-200 p-4">
-        <div className="pb-3 font-bold">
-          Description{" "}
-          {currentDraft?.description &&
-            currentDraft.description !== ticket.description &&
-            "(draft)"}
+        <div className="flex flex-col items-start bg-slate-200 p-4">
+          <div className="pb-3 font-bold">
+            Description{" "}
+            {currentDraft?.description &&
+              currentDraft.description !== ticket.description &&
+              "(draft)"}
+          </div>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) =>
+              isEditingDescription ? (
+                <textarea
+                  {...field}
+                  className="w-full rounded border px-2 py-1"
+                  onBlur={() => setIsEditingDescription(false)}
+                />
+              ) : (
+                <div
+                  className="w-full cursor-pointer"
+                  onClick={() => setIsEditingDescription(true)}
+                >
+                  {ticket.description}
+                </div>
+              )
+            }
+          />
         </div>
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) =>
-            isEditingDescription ? (
-              <textarea
-                {...field}
-                className="w-full rounded border px-2 py-1"
-                onBlur={() => setIsEditingDescription(false)}
-              />
-            ) : (
-              <div
-                className="w-full cursor-pointer"
-                onClick={() => setIsEditingDescription(true)}
-              >
-                {ticket.description}
-              </div>
-            )
-          }
-        />
-      </div>
 
-      <div className="flex flex-col items-start bg-slate-200 p-4">
-        <div className="pb-3 font-bold">
-          Due Date{" "}
-          {currentDraft?.dueDate &&
-            currentDraft.dueDate !== ticket.dueDate &&
-            "(draft)"}
+        <div className="flex flex-col items-start bg-slate-200 p-4">
+          <div className="pb-3 font-bold">
+            Due Date{" "}
+            {currentDraft?.dueDate &&
+              currentDraft.dueDate !== ticket.dueDate &&
+              "(draft)"}
+          </div>
+          <Controller
+            name="dueDate"
+            control={control}
+            render={({ field }) =>
+              isEditingDueDate ? (
+                <input
+                  {...field}
+                  className="w-full rounded border px-2 py-1"
+                  type="date"
+                  value={
+                    field.value instanceof Date
+                      ? field.value.toISOString().split("T")[0]
+                      : field.value
+                  }
+                  onBlur={() => setIsEditingDueDate(false)}
+                />
+              ) : (
+                <div
+                  className="w-full cursor-pointer"
+                  onClick={() => setIsEditingDueDate(true)}
+                >
+                  {ticket.dueDate ? formatDate(ticket.dueDate) : "No due date"}
+                </div>
+              )
+            }
+          />
         </div>
-        <Controller
-          name="dueDate"
-          control={control}
-          render={({ field }) =>
-            isEditingDueDate ? (
-              <input
-                {...field}
-                className="w-full rounded border px-2 py-1"
-                type="date"
-                value={
-                  field.value instanceof Date
-                    ? field.value.toISOString().split("T")[0]
-                    : field.value
-                }
-                onBlur={() => setIsEditingDueDate(false)}
-              />
-            ) : (
-              <div
-                className="w-full cursor-pointer"
-                onClick={() => setIsEditingDueDate(true)}
-              >
-                {ticket.dueDate ? formatDate(ticket.dueDate) : "No due date"}
-              </div>
-            )
-          }
-        />
       </div>
-
+      <div>
+        <div className="py-3 font-bold">History</div>
+      </div>
       <div className="flex h-[160px] flex-col items-start overflow-y-auto bg-slate-200 p-4">
-        <div className="pb-3 font-bold">History</div>
         <div className="flex-grow">
           {ticket.history?.map((history) => (
             <div key={history._id} className="flex flex-col">
