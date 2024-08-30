f2dd916e21aefd24383c0be789476a92550bd664
Monzoor Moshed Mahmud <mmtamal@gmail.com>
2024-08-30 23:38:32 +0600

    fix SSR

--- src/app/components/page-container/container.tsx
+++ src/app/components/page-container/container.tsx
@@ -4,17 +4,28 @@ import { useDragAndDrop } from "@todo/hooks";
 import { CategoryCard } from "../category";
 import { Ticket } from "../ticket";
 import { CreateActions } from "../control-header/control-header";
+import { Categories } from "@todo/types";
+import { useMemo, useState } from "react";
 
-export const Container: React.FC = () => {
+export const Container: React.FC<{
+  data: Categories;
+}> = ({ data }) => {
+  const [categoriesData, setCategoriesData] = useState<Categories>(data);
   const { categories, draggingCategory, onDragStart, onDragOver, onDrop } =
     useDragAndDrop();
+
+  useMemo(() => {
+    if (categories) {
+      setCategoriesData(categories);
+    }
+  }, [categories]);
 
   return (
     <>
       <CreateActions />
       <div className="mx-auto overflow-hidden py-10 pl-5 lg:container">
         <div className="flex items-start gap-3 overflow-auto">
-          {categories.map((category) => (
+          {categoriesData.map((category) => (
             <CategoryCard
               key={category._id}
               header={category.name.toUpperCase()}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { headers } from "next/headers";
 import { Container } from "./components";
+import { Categories } from "@todo/types";
 
 export default function Home() {
-  return <Container />;
+  const headerValues = headers();
+  const categoriesHeader = headerValues.get("x-categories");
+
+  // Activate SSR
+  let categories: Categories = [];
+  if (categoriesHeader !== null) {
+    categories = JSON.parse(categoriesHeader) as Categories;
+  }
+
+  return <Container data={categories} />;
 }
